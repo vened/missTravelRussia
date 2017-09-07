9eb9562307d0d40c248d5ea29d4abc44024c2ef5
maxstbn <maxstbn@gmail.com>
2017-09-07 15:58:57 +0300

    конфиг деплоя

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,7 +1,5 @@
 #!/usr/bin/env puma
 
-#/home/deployTest1/www/missTravelRussia/shared/config
-
 environment "production"
 
 bind "unix:///home/deployTest1/www/missTravelRussia/shared/tmp/sockets/puma.sock"
@@ -9,28 +7,21 @@ pidfile "/home/deployTest1/www/missTravelRussia/shared/tmp/pids/puma.pid"
 state_path "/home/deployTest1/www/missTravelRussia/shared/tmp/sockets/puma.state"
 directory "/home/deployTest1/www/missTravelRussia/current"
 
-# stdout_redirect "/home/deployTest1/www/missTravelRussiashared/log/puma_access.log",
-#                 "/home/deployTest1/www/missTravelRussiashared/log/puma_error.log",
-#                 true
-
-stdout_redirect '/home/deployTest1/www/missTravelRussia/shared/log/stdout', '/home/deployTest1/www/missTravelRussia/shared/log/stderr', true
-# stdout_redirect '/u/apps/lolcat/log/stdout', '/u/apps/lolcat/log/stderr', true
-
+stdout_redirect "/home/deployTest1/www/missTravelRussiashared/log/puma_access.log",
+                "/home/deployTest1/www/missTravelRussiashared/log/puma_error.log",
+                true
 
 workers 10
 threads 1,10
 
-#daemonize true
+daemonize true
 
 activate_control_app "unix:///home/deployTest1/www/missTravelRussia/shared/sockets/pumactl.sock"
 
-# preload_app!
+preload_app!
 
 on_worker_boot do
   ActiveSupport.on_load(:active_record) do
     ActiveRecord::Base.establish_connection
   end
 end
-
-#prune_bundler
-#preload_app!
