712573d07c18297b40f4fcea809c6b863c6c2bd2
maxstbn <maxstbn@gmail.com>
2017-09-12 23:50:19 +0300

    форма редактирвоания профайла

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
 
+  get 'personal_information' => 'pages#personal_information'
+
 end
