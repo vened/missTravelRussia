1821c0d8186d2512bf16d291e2c38eef61ec3b93
maxstbn <maxstbn@gmail.com>
2017-09-19 09:02:45 +0300

    форматирование текста

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def meta_title
-    "Конкурс красоты missTravelRussia"
+    "Конкурс красоты Мисс Туризм 2017"
   end
   def meta_desc
     "Конкурс красоты представительниц индустрии туризма Мисс Туризм 2017."
