d09c4432d2cabf6540b7ede0bab36214372a6cd8
maxstbn <maxstbn@gmail.com>
2017-09-20 09:15:55 +0300

    топ 12

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @users = VotesQuery.new.call().limit(10)
+    @users = VotesQuery.new.call().limit(12)
   end
 
   def personal_information
