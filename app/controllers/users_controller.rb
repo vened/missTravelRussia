fdf239c6a3795f7ab95783c847e722e864ba16ab
maxstbn <maxstbn@gmail.com>
2017-09-20 23:06:40 +0300

    votes inc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,11 +11,14 @@ class UsersController < ApplicationController
     if params[:user_id].present?
       # @users = @users.where(number: params[:user_id].to_f)
 
-      @user_voteable = @users.where({user_voteables: {
-          '$all' => [{'$elemMatch' => {user_voteable_id: params[:user_id]}}]
-      }})
-      @user = @users.find_by(number: params[:user_id])
-      @user.update_attribute(:votes, @user_voteable.length)
+      user_is = @users.where(_role: 'user', is_vote: true)
+
+      user_is.each do |current_user|
+        user_voteable = @users.where({user_voteables: {
+            '$all' => [{'$elemMatch' => {user_voteable_id: "#{current_user.number}"}}]
+        }})
+        current_user.update_attribute(:votes, user_voteable.length)
+      end
     end
     @users = @users.page(params[:page])
     authorize User
