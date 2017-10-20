3460c64c01ec2d086233db9ebd8eba4934763a5d
Максим Столбухин <maxstbn@gmail.com>
2017-10-20 21:27:09 +0300

    отзыв голоса

--- app/services/voteable_service.rb
+++ app/services/voteable_service.rb
@@ -5,13 +5,17 @@ class VoteableService
 
   def voteable(current_user, voteable_user_id)
     user = User.find_by(number: voteable_user_id)
-    unless current_user.user_voteables.where(user_voteable_id: voteable_user_id).exists?
+    current_vote = current_user.user_voteables.where(user_voteable_id: voteable_user_id)
+    unless current_vote.exists?
       current_user.user_voteables.create(
           user_voteable_id: voteable_user_id
       )
       user.inc(votes: 1)
       # заменил на update, для обновления updated_at
       # user.update(votes: user.votes + 1)
+    else
+      current_vote.destroy
+      user.inc(votes: -1)
     end
     return user
   end
