b99c115ea74911b2a6afe37497698cbf2ed18870
Максим Столбухин <maxstbn@gmail.com>
2018-07-08 00:23:20 +0300

    header fix

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module MissTravelRussia
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # config.action_controller.default_url_options = { trailing_slash: true, protocol: ENV["RAILS_ENV"] == 'development' ? '//' : 'https://' }
+    config.action_controller.default_url_options = { protocol: ENV["RAILS_ENV"] == 'development' ? '//' : 'https://' }
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
