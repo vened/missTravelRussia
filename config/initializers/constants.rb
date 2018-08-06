67f9bba4eeb5f8a369c88162abd3c628ec0b8532
Максим Столбухин <maxstbn@gmail.com>
2018-08-06 09:39:49 +0300

    сменил дату окончания регистрации

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,4 @@
-REGISTRATION_END_DATE = "2018-08-07".to_date # последний день регистрации
+REGISTRATION_END_DATE = "2018-08-08".to_date # последний день регистрации
 REGISTRATION_COUNTDOWN = (REGISTRATION_END_DATE - Date.current).to_i # кол-во дней до конца регистрации
 REGISTRATION_AVIALABLE = REGISTRATION_COUNTDOWN > 0 # флаг доступности регистрации
 
