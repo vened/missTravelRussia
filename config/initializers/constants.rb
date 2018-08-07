3a222cbcf6f009b7833a30595856df0ae30c167f
Максим Столбухин <maxstbn@gmail.com>
2018-08-08 00:47:40 +0300

    конец регистрации

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,7 @@
-REGISTRATION_END_DATE = "2018-08-08".to_date # последний день регистрации
+REGISTRATION_END_DATE = "2018-08-07".to_date # последний день регистрации
 REGISTRATION_COUNTDOWN = (REGISTRATION_END_DATE - Date.current).to_i # кол-во дней до конца регистрации
-REGISTRATION_AVIALABLE = REGISTRATION_COUNTDOWN > 0 # флаг доступности регистрации
+REGISTRATION_AVIALABLE = false # флаг доступности регистрации
+
+VOTES_END_DATE = "2018-09-04".to_date # последний день голосования
+VOTES_AVIALABLE = true # последний день голосования
 
